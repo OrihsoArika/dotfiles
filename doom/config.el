3cbeacd324bae323e832116a1e1a5323521e730e
orihsoarika <tionf33@gmail.com>
2025-12-29 18:46:37 -0400

    neio

--- doom/config.el
+++ doom/config.el
@@ -23,7 +23,7 @@
 ;;
 
 (setq doom-font (font-spec :family "BlexMono Nerd Font" :size 18)
-      doom-variable-pitch-font (font-spec :family "Inter" :size 20))
+      doom-variable-pitch-font (font-spec :family "Inter 24pt" :size 24))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
